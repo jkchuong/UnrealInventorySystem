bafe4220a130eaa27ede7e84d146ae3d1b529e60
jkchuong <jkchuong@gmail.com>
2026-04-04 17:48:22 +0200

    Inital Commit

---
+++ Source/Inventory/Public/Player/Inv_PlayerController.h
@@ -0,0 +1,16 @@
+﻿// Copyright JKChuong 2026
+
+#pragma once
+
+#include "CoreMinimal.h"
+
+#include "Inv_PlayerController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class INVENTORY_API AInv_PlayerController : public APlayerController
+{
+	GENERATED_BODY()
+};
